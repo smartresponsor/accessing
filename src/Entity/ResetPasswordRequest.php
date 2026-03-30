99c054d67d8045b9896f3c7f54dfc4dbdf6bb43c
Oleksandr Tishchenko <taa0662621456@gmail.com>
2026-03-30 09:29:11 -0500

    Add reset password request entity for Accessing

---
+++ src/Entity/ResetPasswordRequest.php
@@ -0,0 +1,37 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Entity;
+
+use App\Repository\ResetPasswordRequestRepository;
+use Doctrine\ORM\Mapping as ORM;
+use SymfonyCasts\Bundle\ResetPassword\Model\ResetPasswordRequestInterface;
+use SymfonyCasts\Bundle\ResetPassword\Model\ResetPasswordRequestTrait;
+
+#[ORM\Entity(repositoryClass: ResetPasswordRequestRepository::class)]
+#[ORM\Table(name: 'accessing_reset_password_request')]
+class ResetPasswordRequest implements ResetPasswordRequestInterface
+{
+    use ResetPasswordRequestTrait;
+
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\ManyToOne(targetEntity: Account::class)]
+    #[ORM\JoinColumn(nullable: false, onDelete: 'CASCADE')]
+    private Account $account;
+
+    public function __construct(Account $account, \DateTimeInterface $expiresAt, string $selector, string $hashedToken)
+    {
+        $this->account = $account;
+        $this->initialize($expiresAt, $selector, $hashedToken);
+    }
+
+    public function getAccount(): Account
+    {
+        return $this->account;
+    }
+}
