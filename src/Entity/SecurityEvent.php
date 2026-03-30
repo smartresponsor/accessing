c974622521d198d33a38fb49d0dcd429d45435f0
Oleksandr Tishchenko <taa0662621456@gmail.com>
2026-03-30 08:02:39 -0500

    Add SecurityEvent entity for audit trail in Accessing

---
+++ src/Entity/SecurityEvent.php
@@ -0,0 +1,121 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Entity;
+
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity]
+#[ORM\Table(name: 'accessing_security_event')]
+#[ORM\Index(name: 'idx_accessing_security_event_type', columns: ['event_type'])]
+#[ORM\Index(name: 'idx_accessing_security_event_occurred_at', columns: ['occurred_at'])]
+class SecurityEvent
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\ManyToOne(targetEntity: Account::class)]
+    #[ORM\JoinColumn(nullable: true, onDelete: 'SET NULL')]
+    private ?Account $account = null;
+
+    #[ORM\Column(length: 64, name: 'event_type')]
+    private string $eventType = '';
+
+    #[ORM\Column(type: Types::JSON)]
+    private array $context = [];
+
+    #[ORM\Column(length: 45, nullable: true)]
+    private ?string $ipAddress = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $userAgent = null;
+
+    #[ORM\Column(type: Types::DATETIME_IMMUTABLE, name: 'occurred_at')]
+    private \DateTimeImmutable $occurredAt;
+
+    public function __construct()
+    {
+        $this->occurredAt = new \DateTimeImmutable();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getAccount(): ?Account
+    {
+        return $this->account;
+    }
+
+    public function setAccount(?Account $account): self
+    {
+        $this->account = $account;
+
+        return $this;
+    }
+
+    public function getEventType(): string
+    {
+        return $this->eventType;
+    }
+
+    public function setEventType(string $eventType): self
+    {
+        $this->eventType = trim($eventType);
+
+        return $this;
+    }
+
+    public function getContext(): array
+    {
+        return $this->context;
+    }
+
+    public function setContext(array $context): self
+    {
+        $this->context = $context;
+
+        return $this;
+    }
+
+    public function getIpAddress(): ?string
+    {
+        return $this->ipAddress;
+    }
+
+    public function setIpAddress(?string $ipAddress): self
+    {
+        $this->ipAddress = $ipAddress;
+
+        return $this;
+    }
+
+    public function getUserAgent(): ?string
+    {
+        return $this->userAgent;
+    }
+
+    public function setUserAgent(?string $userAgent): self
+    {
+        $this->userAgent = $userAgent;
+
+        return $this;
+    }
+
+    public function getOccurredAt(): \DateTimeImmutable
+    {
+        return $this->occurredAt;
+    }
+
+    public function setOccurredAt(\DateTimeImmutable $occurredAt): self
+    {
+        $this->occurredAt = $occurredAt;
+
+        return $this;
+    }
+}
