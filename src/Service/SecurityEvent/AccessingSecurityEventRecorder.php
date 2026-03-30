da635dbf5984d999abff18192edc09a1ef31d644
Oleksandr Tishchenko <taa0662621456@gmail.com>
2026-03-30 08:08:26 -0500

    Add security event recorder service for Accessing

---
+++ src/Service/SecurityEvent/AccessingSecurityEventRecorder.php
@@ -0,0 +1,37 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Service\SecurityEvent;
+
+use App\Entity\Account;
+use App\Entity\SecurityEvent;
+use App\ServiceInterface\SecurityEvent\AccessingSecurityEventRecorderInterface;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\RequestStack;
+
+final class AccessingSecurityEventRecorder implements AccessingSecurityEventRecorderInterface
+{
+    public function __construct(
+        private readonly EntityManagerInterface $entityManager,
+        private readonly RequestStack $requestStack,
+    ) {
+    }
+
+    public function record(string $eventType, ?Account $account = null, array $context = []): SecurityEvent
+    {
+        $request = $this->requestStack->getCurrentRequest();
+
+        $securityEvent = (new SecurityEvent())
+            ->setEventType($eventType)
+            ->setAccount($account)
+            ->setContext($context)
+            ->setIpAddress($request?->getClientIp())
+            ->setUserAgent($request?->headers->get('User-Agent'));
+
+        $this->entityManager->persist($securityEvent);
+        $this->entityManager->flush();
+
+        return $securityEvent;
+    }
+}
