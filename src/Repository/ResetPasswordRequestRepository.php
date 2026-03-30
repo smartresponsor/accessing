299254ef5610edc5d73f78906c2325731f027975
Oleksandr Tishchenko <taa0662621456@gmail.com>
2026-03-30 17:15:20 -0500

    Fix reset password repository contract integration

--- src/Repository/ResetPasswordRequestRepository.php
+++ src/Repository/ResetPasswordRequestRepository.php
@@ -7,8 +7,9 @@ namespace App\Repository;
 use App\Entity\ResetPasswordRequest;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\Persistence\ManagerRegistry;
+use SymfonyCasts\Bundle\ResetPassword\Model\ResetPasswordRequestInterface;
 use SymfonyCasts\Bundle\ResetPassword\Persistence\Repository\ResetPasswordRequestRepositoryTrait;
-use SymfonyCasts\Bundle\ResetPassword\Persistence\Repository\ResetPasswordRequestRepositoryInterface;
+use SymfonyCasts\Bundle\ResetPassword\Persistence\ResetPasswordRequestRepositoryInterface;
 
 /**
  * @extends ServiceEntityRepository<ResetPasswordRequest>
@@ -20,5 +21,10 @@ final class ResetPasswordRequestRepository extends ServiceEntityRepository imple
     public function __construct(ManagerRegistry $registry)
     {
         parent::__construct($registry, ResetPasswordRequest::class);
+    }
+
+    public function createResetPasswordRequest(object $user, \DateTimeInterface $expiresAt, string $selector, string $hashedToken): ResetPasswordRequestInterface
+    {
+        return new ResetPasswordRequest($user, $expiresAt, $selector, $hashedToken);
     }
 }
