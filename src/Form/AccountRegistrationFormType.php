6fbbd8cc3d9dd1059a48d44b2ed5010db52a345e
Oleksandr Tishchenko <taa0662621456@gmail.com>
2026-03-30 00:25:48 -0500

    Add Account registration form type for Accessing

---
+++ src/Form/AccountRegistrationFormType.php
@@ -0,0 +1,41 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Form;
+
+use App\Dto\AccountRegistrationRequest;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TelType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+final class AccountRegistrationFormType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('email', EmailType::class)
+            ->add('plainPassword', PasswordType::class)
+            ->add('displayName', TextType::class, [
+                'required' => false,
+            ])
+            ->add('phoneNumber', TelType::class, [
+                'required' => false,
+            ])
+            ->add('submit', SubmitType::class, [
+                'label' => 'Create account',
+            ]);
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => AccountRegistrationRequest::class,
+        ]);
+    }
+}
