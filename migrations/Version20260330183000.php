3aff6092f0f29ce4f623626c50fd3a57e603015c
Oleksandr Tishchenko <taa0662621456@gmail.com>
2026-03-30 12:39:59 -0500

    Add migration for Account TOTP secret in Accessing

---
+++ migrations/Version20260330183000.php
@@ -0,0 +1,26 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+final class Version20260330183000 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return 'Add TOTP secret column to the Accessing account table.';
+    }
+
+    public function up(Schema $schema): void
+    {
+        $this->addSql('ALTER TABLE accessing_account ADD totp_secret VARCHAR(255) DEFAULT NULL');
+    }
+
+    public function down(Schema $schema): void
+    {
+        $this->addSql('ALTER TABLE accessing_account DROP totp_secret');
+    }
+}
