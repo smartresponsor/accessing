ceb7a567f89b5ecfafcf129b33886170afdaefaf
Oleksandr Tishchenko <taa0662621456@gmail.com>
2026-03-31 01:13:28 -0500

    Update email verification controller test for POST request flow

--- tests/Functional/AccessingEmailVerificationControllerTest.php
+++ tests/Functional/AccessingEmailVerificationControllerTest.php
@@ -8,10 +8,18 @@ use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 final class AccessingEmailVerificationControllerTest extends WebTestCase
 {
-    public function testEmailVerificationRequestRequiresAuthentication(): void
+    public function testEmailVerificationRequestGetIsNotAllowed(): void
     {
         $client = static::createClient();
         $client->request('GET', '/verification/email/request');
+
+        self::assertResponseStatusCodeSame(405);
+    }
+
+    public function testEmailVerificationRequestPostRequiresAuthentication(): void
+    {
+        $client = static::createClient();
+        $client->request('POST', '/verification/email/request');
 
         self::assertResponseRedirects('/login');
     }
