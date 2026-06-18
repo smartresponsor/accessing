5bb29826f68d33f48b822faf62a1e8a09de85bec
Oleksandr Tishchenko <taa0662621456@gmail.com>
2026-06-18 14:50:04 -0500

    Add Access recovery request page service

---
+++ src/Service/Http/Access/AccessRecoveryRequestService.php
@@ -0,0 +1,32 @@
+<?php
+
+# Copyright (c) 2025 Oleksandr Tishchenko / Marketing America Corp
+declare(strict_types=1);
+
+namespace App\Accessing\Service\Http\Access;
+
+use App\Accessing\ServiceInterface\Rendering\AccessPageResponderInterface;
+use App\Accessing\ServiceInterface\Rendering\AccessPageViewFactoryInterface;
+use App\Interfacing\Contract\Surface\InterfaceSurfaceRenderableInterface;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\FormFactoryInterface;
+use Symfony\Component\HttpFoundation\Response;
+
+final readonly class AccessRecoveryRequestService
+{
+    public function __construct(
+        private FormFactoryInterface $formFactory,
+        private AccessPageViewFactoryInterface $pageViewFactory,
+        private AccessPageResponderInterface $pageResponder,
+    ) {
+    }
+
+    public function __invoke(): Response|InterfaceSurfaceRenderableInterface
+    {
+        $form = $this->formFactory->createBuilder()
+            ->add('email', EmailType::class)
+            ->getForm();
+
+        return $this->pageResponder->respond($this->pageViewFactory->requestRecovery($form->createView()));
+    }
+}
