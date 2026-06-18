491e43dad0b8032ca5d17bd10bd01b711a87ba4b
Oleksandr Tishchenko <taa0662621456@gmail.com>
2026-06-18 14:46:16 -0500

    Add Access security event service

---
+++ src/Service/Http/Access/AccessSecurityEventsService.php
@@ -0,0 +1,29 @@
+<?php
+
+# Copyright (c) 2025 Oleksandr Tishchenko / Marketing America Corp
+declare(strict_types=1);
+
+namespace App\Accessing\Service\Http\Access;
+
+use App\Accessing\RepositoryInterface\AccessSecurityEventRepositoryInterface;
+use App\Accessing\ServiceInterface\Rendering\AccessPageResponderInterface;
+use App\Accessing\ServiceInterface\Rendering\AccessPageViewFactoryInterface;
+use App\Interfacing\Contract\Surface\InterfaceSurfaceRenderableInterface;
+use Symfony\Component\HttpFoundation\Response;
+
+final readonly class AccessSecurityEventsService
+{
+    public function __construct(
+        private AccessSecurityEventRepositoryInterface $securityEventRepository,
+        private AccessPageViewFactoryInterface $pageViewFactory,
+        private AccessPageResponderInterface $pageResponder,
+    ) {
+    }
+
+    public function __invoke(): Response|InterfaceSurfaceRenderableInterface
+    {
+        return $this->pageResponder->respond($this->pageViewFactory->securityEvents(
+            $this->securityEventRepository->findRecentEvents(),
+        ));
+    }
+}
