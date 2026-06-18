5af1a57d5f0047b3ce4b270a64938c9d8fde6593
Oleksandr Tishchenko <taa0662621456@gmail.com>
2026-06-18 14:48:54 -0500

    Add Access sign in page service

---
+++ src/Service/Http/Access/AccessSignInService.php
@@ -0,0 +1,34 @@
+<?php
+
+# Copyright (c) 2025 Oleksandr Tishchenko / Marketing America Corp
+declare(strict_types=1);
+
+namespace App\Accessing\Service\Http\Access;
+
+use App\Accessing\ServiceInterface\Rendering\AccessPageResponderInterface;
+use App\Accessing\ServiceInterface\Rendering\AccessPageViewFactoryInterface;
+use App\Interfacing\Contract\Surface\InterfaceSurfaceRenderableInterface;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\FormFactoryInterface;
+use Symfony\Component\HttpFoundation\Response;
+
+final readonly class AccessSignInService
+{
+    public function __construct(
+        private FormFactoryInterface $formFactory,
+        private AccessPageViewFactoryInterface $pageViewFactory,
+        private AccessPageResponderInterface $pageResponder,
+    ) {
+    }
+
+    public function __invoke(): Response|InterfaceSurfaceRenderableInterface
+    {
+        $form = $this->formFactory->createBuilder()
+            ->add('email', EmailType::class)
+            ->add('plainPassword', PasswordType::class)
+            ->getForm();
+
+        return $this->pageResponder->respond($this->pageViewFactory->signIn($form->createView()));
+    }
+}
