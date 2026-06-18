846ec7fff7e154012a8439c27d58c01c6e4e63b8
Oleksandr Tishchenko <taa0662621456@gmail.com>
2026-06-18 14:43:59 -0500

    Add Access operator detail service

---
+++ src/Service/Http/Access/AccessOperatorUserDetailService.php
@@ -0,0 +1,39 @@
+<?php
+
+# Copyright (c) 2025 Oleksandr Tishchenko / Marketing America Corp
+declare(strict_types=1);
+
+namespace App\Accessing\Service\Http\Access;
+
+use App\Accessing\RepositoryInterface\AccessRepositoryInterface;
+use App\Accessing\RepositoryInterface\AccessSecurityEventRepositoryInterface;
+use App\Accessing\ServiceInterface\Rendering\AccessPageResponderInterface;
+use App\Accessing\ServiceInterface\Rendering\AccessPageViewFactoryInterface;
+use App\Interfacing\Contract\Surface\InterfaceSurfaceRenderableInterface;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+
+final readonly class AccessOperatorUserDetailService
+{
+    public function __construct(
+        private AccessRepositoryInterface $userRepository,
+        private AccessSecurityEventRepositoryInterface $securityEventRepository,
+        private AccessPageViewFactoryInterface $pageViewFactory,
+        private AccessPageResponderInterface $pageResponder,
+    ) {
+    }
+
+    public function __invoke(int $id): Response|InterfaceSurfaceRenderableInterface
+    {
+        $user = $this->userRepository->findById($id);
+
+        if (null === $user) {
+            throw new NotFoundHttpException();
+        }
+
+        return $this->pageResponder->respond($this->pageViewFactory->operatorUserDetail(
+            $user,
+            $this->securityEventRepository->findRecentEventsForUser($user),
+        ));
+    }
+}
