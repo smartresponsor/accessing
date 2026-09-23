f826a02dc243f49e8c4272e498dbd0d0eb2bd827
SmartResponsor Development <dev@smartresponsor.com>
2026-09-23 12:22:24 -0500

    Adopt Objecting identity constraint policy

--- config/bundles.php
+++ config/bundles.php
@@ -14,6 +14,7 @@ return [
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
     Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
     Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
+    App\Objecting\ObjectBundle::class => ['all' => true],
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
     Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
     Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
